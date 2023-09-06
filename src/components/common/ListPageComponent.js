a8b3f76f9747e1ab1c21c1fbdaf0be8d8c9b0e24
JY LEE <wndyd0110@gmail.com>
2023-09-06 21:55:02 +0900

    페이징 구현 진행 중

--- src/components/common/ListPageComponent.js
+++ src/components/common/ListPageComponent.js
@@ -1,22 +1,47 @@
 import { Button, CardFooter, IconButton } from "@material-tailwind/react";
 
-const ListPageComponent = ({ movePage,startNum, endNum, prevBtn, nextBtn, pageNums, page }) => {
+const ListPageComponent = ({
+  movePage,
+  startNum,
+  endNum,
+  prevBtn,
+  nextBtn,
+  pageNums,
+  page,
+}) => {
+
+  const handleClickPage = (pageNum) => {
+
+    movePage(pageNum)
+
+  }
+
   return (
     <div>
       <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-        
-        <Button variant="outlined" size="sm">
-          Previous
-        </Button>
+        {prevBtn ? (
+          <Button variant="outlined" size="sm">
+            Previous
+          </Button>
+        ) : (
+          <span></span>
+        )}
 
-        <div className="flex items-center gap-2">
-          <IconButton variant="outlined" size="sm">
-            1
-          </IconButton>
+        <div className="flex items-center gap-2" >
+          {pageNums.map((pageNum) => (
+            <IconButton key ={pageNum} variant="outlined" size="sm" onClick={handleClickPage}>
+              {pageNum}
+            </IconButton>
+          ))}
         </div>
-        <Button variant="outlined" size="sm">
-          Next
-        </Button>
+
+        {nextBtn ? (
+          <Button variant="outlined" size="sm">
+            Next
+          </Button>
+        ) : (
+          <span></span>
+        )}
       </CardFooter>
     </div>
   );
